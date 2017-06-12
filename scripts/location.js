7cc07b3286efcfe584c9d5dfd4d2d15ba01575fc
ZhouRM <jnws1995@outlook.com>
2017-06-13 00:11:07 +0800

    alter the canvas

--- scripts/location.js
+++ scripts/location.js
@@ -3,6 +3,20 @@ window.onload=function(){
 	Canvas();
 }
 
+// 2D图形
+function Canvas(){
+	var c = document.getElementById("drew");
+	var ctx = c.getContext("2d");
+	ctx.beginPath();
+	ctx.arc(95,50,40,0.2*Math.PI);
+	ctx.stroke();
+	
+	var grd=ctx.createLinearGradient(0,0,200,0);
+	grd.addColorStop(0,"bule");
+	grd.addColorStop(1,"white");
+	ctx.fillStyle=grd;
+	ctx.fillRect(95,50,40,0.2*Math.PI);
+}
 
 
 // 地图API
@@ -30,99 +44,6 @@ function BDmap(){
 			alert('failed'+this.getStatus());
 		}        
 	},{enableHighAccuracy: true})
-}
-
-
-
-// 2D图形
-function Canvas(){
-	var canvas,ctx;
-	var clockRadius=250;
-	var clockImage;
-	function  clear(){
-    	ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
-	}
-    	function drawScene(){
-	    claer();
-	    var date = new Date();
-	    var hours = date.getHours();
-	    var minutes= date.getMinutes();
-	    var seconds = date.getSeconds();
-
-	    hours = hours>12?hours-12:hours;
-	    var hour = hours+minutes/60;
-	    var minute = minutes+ seconds/60;
-
-	    ctx.save();
-	    ctx.drawImage(clockImage,0,0,500,500);
-	    ctx.translate(canvas.width/2,canvas.height/2);
-	    ctx.beginPath();
-
-	    ctx.font="36px Arial";
-	    ctx.fillStyle="#000";
-	    ctx.textAlign="center";
-	    ctx.textBaseline = "middle";
-	    for(var n=1;n<=12;n++){
-		var theta = (n-3)*(Math.PI*2)/12;
-		var x=clockRadius*0.7*Math.cos(theta);
-		var y=clockRadius*0.7*Math.sin(theta);
-		ctx.fillText(n,x,y);
-	    }
-
-	    ctx.save();
-	    var theta = (hour-3)*2*Math.PI/12;
-	    ctx.rotate(theta);
-	    ctx.beginPath();
-	    ctx.moveTo(-15,-5);
-	    ctx.lineTo(-15,5);
-	    ctx.lineTo(clockRadius*0.5,1);
-	    ctx.lineTo(clockRadius*0.5,-1);
-	    ctx.fill();
-	    ctx.restore();
-
-	    ctx.save();
-	    var theta=(minute-15)*2*Math.PI/60;
-	    ctx.rotate(theta);
-	    ctx.beginPath();
-	    ctx.moveTo(-15,-4);
-	    ctx.lineTo(-15,4);
-	    ctx.lineTo(clockRadius*0.8,1);
-	    ctx.lineTo(clockRadius*0.8,-1);
-	    ctx.fill();
-	    ctx.restore();
-
-	    ctx.save();
-	    var theta = (seconds-15)*2*Math.PI/60;
-	    ctx.rotate(theta);
-	    ctx.beginPath();
-	    ctx.moveTo(-15,-4);
-	    ctx.lineTo(-15,4);
-	    ctx.lineTo(clockRadius*0.8,1);
-	    ctx.lineTo(clockRadius*0.8,-1);
-	    ctx.fill();
-	    ctx.restore();
-
-	    ctx.save();
-	    var theta = (seconds-15)*2*Math.PI/60;
-	    ctx.rotate(theta);
-	    ctx.beginPath();
-	    ctx.moveTo(-15,-3);
-	    ctx.lineTo(-15,3);
-	    ctx.lineTo(clockRadius*0.9,1);
-	    ctx.lineTo(clockRadius*0.9,-1);
-	    ctx.fillStyle="#0f0";
-	    ctx.fill();
-	    ctx.restore();
-	    ctx.restore();
-	}
-	
-	$(function(){
-	    canvas=document.getElementById("drew");
-	    ctx=canvas.getContext('2d');
-	    clockImage=new Image();
-	    clockImage.src='images/';
-	    setInterval(drawScene,1000);
-	});
 }
 
 
