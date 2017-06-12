d2408ec160903b3befc720145866242d724cbe1f
ZhouRM <jnws1995@outlook.com>
2017-06-13 00:59:53 +0800

    Create location.js

--- scripts/location.js
+++ scripts/location.js
@@ -10,12 +10,8 @@ function Canvas(){
 	ctx.beginPath();
 	ctx.arc(95,50,40,0.2*Math.PI);
 	ctx.stroke();
-	
-	//var grd=ctx.createLinearGradient(0,0,200,0);
-	//grd.addColorStop(0,"bule");
-	//grd.addColorStop(1,"white");
-	//ctx.fillStyle=grd;
-	//ctx.fillRect(95,50,40,0.2*Math.PI);
+	ctx.fillStyle="#FF0000";
+	ctx.fillRect(0,0,50,75);
 }
 
 
